b1a33921f5dcd9207bc78c0b19daec0401e1e10d
Adriano Ruseler <adrianoruseler@gmail.com>
2024-09-13 08:44:49 -0300

    Create defaults-dist.php

---
+++ moodle/debian-12-dev/defaults-dist.php
@@ -0,0 +1,21 @@
+<?php
+$defaults['moodle']['summary'] = 'Moodle QA Site! '; // for core settings
+$defaults['moodle']['custommenuitems'] = 'Theme
+-Boost | ?theme=boost
+-Classic | ?theme=classic
+';
+// $defaults['moodle']['allowthemechangeonurl'] = 1; // TODO.
+$defaults['moodle']['timezone'] = 'America/Sao_Paulo';
+$defaults['moodle']['defaultcity'] = 'Curitiba';
+
+$defaults['moodle']['pathtophp'] = '/opt/bitnami/php/bin/php';
+$defaults['moodle']['pathtodu'] = '/usr/bin/du';
+$defaults['moodle']['aspellpath'] = '/usr/bin/aspell';
+$defaults['moodle']['pathtogs'] = '/usr/bin/gs';
+$defaults['moodle']['pathtodot'] = '/usr/bin/dot';
+$defaults['moodle']['pathtopdftoppm'] = '/usr/bin/pdftoppm';
+$defaults['moodle']['pathtopython'] = '/usr/bin/python3';
+
+$defaults['moodle']['auth_instructions'] = 'Usuário: user
+Senha: bitnami';
+$defaults['moodle']['forcelogin'] = 1;
